fea46cf0f3755aa5db12c1704a0d0a6343d934cd
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-02-17 19:51:09 -0500

    event tests lst

--- src/__tests__/list-item.test.js
+++ src/__tests__/list-item.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ListItem } from '../components/list-item';
 import { shallow } from 'enzyme';
+import { handleExpandedItem } from '../actions/display';
+
 import '../configure-tests'
 
 describe('<ListItem /> component', () => {
@@ -17,18 +19,19 @@ describe('<ListItem /> component', () => {
       countryOfOrigin: 'USA',
     }
     const wrapper = shallow(<ListItem {...singleObjectData} />)
-    
+
     expect(wrapper.contains(
       <p>12345</p>)).toEqual(true);
     expect(wrapper.contains(<h3>thing</h3>)).toEqual(true);
     expect(wrapper.contains(<p>USA</p>)).toEqual(true);
   })
-  it( 'simulates click events', ()=>{
-
-    // const id = 10
-    // const wrapper = shallow(<ListItem id={id}/>)
-    // wrapper.find('button').simulate('click')
-    // expect(wrapper.id()).toEqual(id)
+  it('simulates click events', () => {
+    const fakeDispatch = jest.fn();
+    const id = 10
+    const wrapper = shallow(<ListItem dispatch={fakeDispatch} id={id} />)
+    wrapper.find('button').simulate('click')
+    console.log(wrapper.debug())
+    expect(fakeDispatch).toHaveBeenCalledWith(handleExpandedItem(id))
   })
 
 }) 
--- src/components/list-item.js
+++ src/components/list-item.js
@@ -5,7 +5,6 @@ import { withRouter } from 'react-router-dom';
 
 export class ListItem extends React.Component {
   expandThisItem() {
-
     this.props.dispatch(handleExpandedItem(this.props.id))
   }
 
@@ -15,7 +14,7 @@ export class ListItem extends React.Component {
         <div className='contracted image-container'>
           <img className='image-thumbnail' src={this.props.imageOne}
             alt={this.props.name}
-            onClick={(e) => this.expandThisItem(e.target)}
+            onClick={() => this.expandThisItem()}
           />
         </div>
         <div className='contracted info-container'>
@@ -28,8 +27,9 @@ export class ListItem extends React.Component {
         <div className='contracted button-container'>
           <button className='contracted-button'
             value={this.props.id}
-            onClick={(e) => {
-              this.expandThisItem(e.target)
+            onClick={() => {
+              console.log();
+              this.expandThisItem()
             }}>
             More</button>
         </div>
